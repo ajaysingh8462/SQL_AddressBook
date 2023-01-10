108d513cc81f27edad531ddf6dbba7095d39734a
ajaysingh8462 <ajaysingh84625@gmail.com>
2023-01-10 16:39:25 +0530

    [ajay]-Creating database using sqlclint

---
+++ AddressBookSystem/AddressBookDataBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class AddressBookDataBase
+    {
+        public static void CreateDatabass()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-P65PD0V;Initial catalog=master;Integrated Security=true");
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("Create database AddressBook", connection);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Databass ctearted");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
